e40449117e6195fc9ba51de8836448b5f9f79cd2
AshkanZj <ashkanzojaji19@gmail.com>
2024-04-18 10:20:00 +0330

    feat: add login validation to router

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -1,60 +1,135 @@
 import Navigo from "navigo";
-import { changePage } from "@/utils";
-import { HomePage, OnBoarding } from "@/pages";
+import { changePage, loginValidation } from "@/utils";
+import {
+  BrandPage,
+  CartPage,
+  CheckoutPage,
+  HomePage,
+  LoginPage,
+  MostPopularPage,
+  OnBoarding,
+  OrdersPage,
+  SearchPage,
+  SingleProductPage,
+  WishlistPage,
+} from "@/pages";
 import { ROUTE } from "@/constant/routes";
-import { Login } from "@/templates/login";
 import { getData } from "@/api/GET/get";
 import Cookies from "js-cookie";
-import { SearchSection } from "@/templates";
-import { wishlist } from "@/templates/Home/wishlist";
+import { Payment } from "@/templates/Checkout/Payment";
+import { Address } from "@/templates/Checkout/Address";
+import { Shipping } from "@/templates/Checkout/Shipping";
 
 const info = {
-  name: "Ashkan",
+  name: "",
   img: "./images/profile.jpg",
 };
 
 export const router = new Navigo("/");
-
-// validation to check if login
-const validation = () => {};
 
 router
   .on(ROUTE.onBoarding, () => {
     changePage(OnBoarding);
   })
   .on(ROUTE.login, () => {
-    changePage(Login);
+    loginValidation().then((valid) => {
+      if (valid) {
+        router.navigate(ROUTE.home);
+      } else changePage(LoginPage);
+    });
   })
   .on(ROUTE.home, () => {
-    getData(`users?email=${Cookies.get("shoea")}`).then((data) => {
-      info.name = data[0].name;
-      changePage(HomePage, info);
+    loginValidation().then((valid) => {
+      if (valid) {
+        getData(`users?email=${Cookies.get("shoea")}`).then((data) => {
+          info.name = data[0].name;
+          changePage(HomePage, info);
+        });
+      } else router.navigate(ROUTE.login);
     });
   })
   .on(ROUTE.wishlist, () => {
-    changePage(wishlist);
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(WishlistPage);
+      } else router.navigate(ROUTE.login);
+    });
   })
   .on(ROUTE.search, () => {
-    changePage(SearchSection);
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(SearchPage);
+      } else router.navigate(ROUTE.login);
+    });
   })
   .on("/products/:id", (res) => {
-    getData(`products/${res.data.id}`).then((data) => {
-      changePage(SingleProduct, data);
+    loginValidation().then((valid) => {
+      if (valid) {
+        getData(`products/${res.data.id}`).then((data) => {
+          changePage(SingleProductPage, data);
+        });
+      } else router.navigate(ROUTE.login);
     });
   })
   .on("/brand/:brand", (res) => {
-    getData(`products?brand=${res.data.brand.toUpperCase()}`).then((data) => {
-      changePage(brandPage, res.data.brand, data);
+    loginValidation().then((valid) => {
+      if (valid) {
+        getData(`products?brand=${res.data.brand.toUpperCase()}`).then(
+          (data) => {
+            changePage(BrandPage, res.data.brand, data);
+          }
+        );
+      } else router.navigate(ROUTE.login);
     });
   })
   .on(ROUTE.mostPopular, () => {
-    changePage(mostPopular);
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(MostPopularPage);
+      } else router.navigate(ROUTE.login);
+    });
   })
   .on(ROUTE.cart, () => {
-    changePage(Cart);
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(CartPage);
+      } else router.navigate(ROUTE.login);
+    });
+  })
+  .on(ROUTE.checkout, () => {
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(CheckoutPage);
+      } else router.navigate(ROUTE.login);
+    });
+  })
+  .on(ROUTE.payment, () => {
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(Payment);
+      } else router.navigate(ROUTE.login);
+    });
+  })
+  .on(ROUTE.shippingAddress, () => {
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(Address);
+      } else router.navigate(ROUTE.login);
+    });
+  })
+  .on(ROUTE.shippingMethod, () => {
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(Shipping);
+      } else router.navigate(ROUTE.login);
+    });
   })
   .on(ROUTE.orders, () => {
-    changePage(Orders);
+    loginValidation().then((valid) => {
+      if (valid) {
+        changePage(OrdersPage);
+      } else router.navigate(ROUTE.login);
+    });
   })
   .on(ROUTE.notFound, () => {
     changePage(NotFound);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export * from "./change-page";
 export * from "./create-element";
 export * from "./deleteFromWL";
+export * from "./login-validation";
